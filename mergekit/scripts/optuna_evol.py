e5bd44e949f340ab325a9b3f30edfc55ca061b48
Luke Meyers <functor.soup@gmail.com>
2024-03-31 18:19:06 -0400

    Corrections post testing

--- mergekit/scripts/optuna_evol.py
+++ mergekit/scripts/optuna_evol.py
@@ -9,21 +9,21 @@ from mergekit.merge import run_merge
 from mergekit.options import MergeOptions, add_merge_options
 
 multiline_yaml_template = """
- models:
-  - model: meta-llama/Llama-2-7b-hf
-    #no parameters necessary for base model
-  - model: meta-llama/Llama-2-7b-chat-hf
-    parameters:
-      density: 0.5
-      weight: 0.5
-  - model: epfl-llm/meditron-7b
-    parameters:
-      density: 0.5
-      weight: 0.5
-  - model: arcee-ai/no_zero_1_step_1000
-    parameters:
-      density: 0.5
-      weight: 0.5
+models:
+ - model: meta-llama/Llama-2-7b-hf
+   #no parameters necessary for base model
+ - model: meta-llama/Llama-2-7b-chat-hf
+   parameters:
+     density: 0.5
+     weight: 0.5
+ - model: epfl-llm/meditron-7b
+   parameters:
+     density: 0.5
+     weight: 0.5
+ - model: arcee-ai/no_zero_1_step_1000
+   parameters:
+     density: 0.5
+     weight: 0.5
 
 merge_method: ties
 base_model: meta-llama/Llama-2-7b-hf
@@ -42,10 +42,15 @@ def create_objective(string_template):
 
     def objective(trial):
         for i in merge_config.models:
+            if i.parameters is None:
+                continue
             for parameter_key, value in i.parameters.items():
                 model_name = i.model.model.path
                 key = f"{model_name}/{parameter_key}"
-                value.value = trial.suggest_uniform(key, 0.0, 1.0)
+                value = trial.suggest_float(key, 0.0, 1.0)
+                i.parameters[parameter_key] = value
+
+        print(merge_config.to_yaml())
 
         with tempfile.TemporaryDirectory() as temp_dir:
             run_merge(
