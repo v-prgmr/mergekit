dc6cadc5798f28cfcbe29277987f3448e31cef73
Luke Meyers <functor.soup@gmail.com>
2024-03-31 19:20:49 -0400

    Crappy code that works for now

--- mergekit/scripts/optuna_evol.py
+++ mergekit/scripts/optuna_evol.py
@@ -35,49 +35,44 @@ dtype: float16
 """
 
 
-def create_objective(string_template):
+def objective(trial):
     merge_config: config.MergeConfiguration = config.MergeConfiguration.model_validate(
-        yaml.safe_load(string_template)
+        yaml.safe_load(multiline_yaml_template)
     )
+    for i in merge_config.models:
+        if i.parameters is None:
+            continue
+        for parameter_key, value in i.parameters.items():
+            model_name = i.model.model.path
+            key = f"{model_name}/{parameter_key}"
+            value = trial.suggest_float(key, 0.0, 1.0)
+            i.parameters[parameter_key] = value
 
-    def objective(trial):
-        for i in merge_config.models:
-            if i.parameters is None:
-                continue
-            for parameter_key, value in i.parameters.items():
-                model_name = i.model.model.path
-                key = f"{model_name}/{parameter_key}"
-                value = trial.suggest_float(key, 0.0, 1.0)
-                i.parameters[parameter_key] = value
+    print(merge_config.to_yaml())
 
-        print(merge_config.to_yaml())
+    with tempfile.TemporaryDirectory() as temp_dir:
+        run_merge(
+            merge_config,
+            temp_dir,
+            options=MergeOptions(),
+        )
 
-        with tempfile.TemporaryDirectory() as temp_dir:
-            run_merge(
-                merge_config,
-                temp_dir,
-                options=MergeOptions(),
-            )
+        results = evaluator.simple_evaluate(
+            model="hf",
+            model_args=f"pretrained={temp_dir}",
+            batch_size=1,
+            tasks=["medqa_4options"],
+            device="cuda",
+        )
 
-            results = evaluator.simple_evaluate(
-                model="hf",
-                model_args=f"pretrained={temp_dir}",
-                batch_size=1,
-                tasks=["medqa_4options"],
-                device="cuda",
-            )
+        print(results["results"])
 
-            print(results["results"])
-
-            return results["results"]["medqa_4options"]["acc,none"]
-
-    return objective
+        return results["results"]["medqa_4options"]["acc,none"]
 
 
 if __name__ == "__main__":
     sampler = optuna.samplers.CmaEsSampler()
     study = optuna.create_study(sampler=sampler)
-    objective = create_objective(multiline_yaml_template)
     study.optimize(objective, n_trials=2)
     best_params = study.best_params
     print(best_params)
